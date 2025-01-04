2b795937208c1b0a24a3c670c64f8d23106eb75e
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2025-01-04 13:42:47 -0300

    refactor: organize folder strucuture

--- main.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_area_2d_new_food(new_food: Food) -> void:
-	GlobalDragging.start(new_food)
--- scenes/Main/area_comandas.gd
+++ scenes/Main/area_comandas.gd
@@ -3,7 +3,7 @@ extends Node2D
 var quant_comanda = 0
 
 func _on_timer_timeout() -> void:
-	var new_comanda = preload("res://scenes/comanda.tscn").instantiate()
+	var new_comanda = preload("res://scenes/Comanda/comanda.tscn").instantiate()
 	
 	new_comanda.position = Vector2(-26.24, 5 + quant_comanda * 80)
 	quant_comanda += 1
