9e93c659b5ca8210e8d5a7ba7432686981c16d6e
João Calafange <jotave369@gmail.com>
2025-01-15 12:38:34 -0300

    cook book agr mostrando nutrição dos ingredientes

--- scenes/receitas/janela_receita.gd
+++ scenes/receitas/janela_receita.gd
@@ -1,4 +1,13 @@
 extends Control
+# Referências aos nodes recipes e nutrition
+@onready var recipes = $recipes
+@onready var nutrition = $Nutrition
 
+func trocar_pagina():
+	# Alterna a visibilidade dos dois nodes
+	recipes.visible = not recipes.visible
+	nutrition.visible = not nutrition.visible
+	
 func fechar_janela():
 	self.visible = false  # Oculta a janela
+	
