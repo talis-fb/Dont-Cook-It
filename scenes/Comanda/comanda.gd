da6e34d0d6b311a048adcafbd3411ba672d6a945
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2025-01-24 12:16:07 -0300

    feat: add score rating based on get_tasty only

--- scenes/Comanda/comanda.gd
+++ scenes/Comanda/comanda.gd
@@ -1,12 +1,15 @@
 #comanda
 extends Node2D
 
+signal dropped(food: Food)
+
 @export var _requested_food: Food
 
 func _ready() -> void:
 	$Dropable.setup()
 	$ShowOnHoverDrop.setup()
-
+	
 func _on_dropable_dropped(food: Food) -> void:
+	dropped.emit(food)
 	print("Command entregue!")
 	self.queue_free()
