ddb1c145cfaf6fba2d34bfa964b2b702a60ceb30
João Calafange <jotave369@gmail.com>
2025-01-28 22:57:53 -0300

    adcionado restrições para as comandas

--- scenes/Comanda/comanda.gd
+++ scenes/Comanda/comanda.gd
@@ -6,9 +6,23 @@ signal dropped(food: Food)
 @export var _requested_food: Food
 
 func _ready() -> void:
+	# Configurar elementos visuais aleatórios
+	var opcoes = [
+		{"texto": "vegano", "cor": Color.DARK_GREEN},
+		{"texto": "lactose", "cor": Color.GOLD},
+		{"texto": "colesterol", "cor": Color.HOT_PINK}
+	]
+	
+	var escolha = opcoes.pick_random()
+	
+	# Aplicar configurações
+	$Label.text = escolha["texto"]
+	$Polygon2D.color = escolha["cor"]
+	
+	# Configurar componentes
 	$Dropable.setup()
 	$ShowOnHoverDrop.setup()
-	
+
 func _on_dropable_dropped(food: Food) -> void:
 	dropped.emit(food)
 	print("Command entregue!")
